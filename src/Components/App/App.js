dedd92523937e47055c2d65866db778be95319eb
kaybekov <kayumbekow@gmail.com>
2022-11-27 10:24:56 +0100

    Add new Components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 
+import Playlist from '../Playlist/Playlist';
 class App extends React.Component {
   render() {
     return (
@@ -9,11 +12,11 @@ class App extends React.Component {
           Ja<span className='highlight'>mmm</span>ing
         </h1>
         <div className='App'>
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className='App-playlist'>
-            {/* <SearchResults /> */}
+            <SearchResults />
 
-            {/* <Playlist /> */}
+            <Playlist />
           </div>
         </div>
       </div>
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Tracklist.css';
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        
+      </div>
+    );
+  }
+}
+
+export default TrackList;
