44b6c76093f0a06fc3c0d39fab4f369df8554dde
kaybekov <kayumbekow@gmail.com>
2022-11-27 11:55:47 +0100

    Add new methods to App component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Playlist.css';
 
+import TrackList from '../TrackList/TrackList';
 class Playlist extends React.Component {
   render() {
     return (
-      <div className='Playlist'>
+      <div className="Playlist">
         <input defaultValue={'New Playlist'} />
-        {/* <TrackList /> */}
-        <button className='Playlist-save'>SAVE TO SPOTIFY</button>
+        <TrackList tracks={this.props.playlistTracks} />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
   }
