7046c057bfee07c85cae7ed0792d19e8c5b95b6b
Kay Bekov <kayumbekow@gmail.com>
2022-12-16 12:55:17 +0100

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -69,7 +69,7 @@ const Spotify = {
           .then((jsonResponse) => {
             const playlistId = jsonResponse.id;
             return fetch(
-              `https://api/spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks
             `,
               {
                 headers: headers,
